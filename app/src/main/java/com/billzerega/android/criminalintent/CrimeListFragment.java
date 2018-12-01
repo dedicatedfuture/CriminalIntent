d903e3c3275016b5c8ce15bbefa2b65e9e9d5a03
William Zerega <zerega85@gmail.com>
2018-12-01 14:42:12 -0500

    implemented pager view 11

--- app/src/main/java/com/billzerega/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/billzerega/android/criminalintent/CrimeListFragment.java
@@ -33,8 +33,8 @@ public class CrimeListFragment extends ListFragment {
         //get the crime from the adapter
         Crime c = ((CrimeAdapter)getListAdapter()).getItem(position);
 
-        //Start CrimeActivity
-        Intent i = new Intent(getActivity(), CrimeActivity.class);
+        //Start CrimePagerActivity with this crime
+        Intent i = new Intent(getActivity(), CrimePagerActivity.class);
         i.putExtra(CrimeFragment.EXTRA_CRIME_ID, c.getId());
         startActivity(i);
     }
